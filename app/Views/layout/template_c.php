3ff6340b950a9238349c92f05c3f71a5586f1803
Mauricio-Varas <mauriciovarasaraya1@gmail.com>
2025-01-12 08:01:24 -0300

    Cambios

--- app/Views/layout/template_c.php
+++ app/Views/layout/template_c.php
@@ -6,11 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Base Template</title>
 
-    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
-    <!-- DataTables CSS -->
     <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link href="<?= base_url('css/home.css'); ?>" rel="stylesheet">
 </head>
@@ -20,19 +18,15 @@
         <?= $this->renderSection('content'); ?>
     </div>
 
-    <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     
-    <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
     
-    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script>
         $(document).ready(function () {
-            // Configurar DataTable para la tabla de responsables
             $('#responsablesTable').DataTable({
                 "language": {
                     "lengthMenu": "Mostrar _MENU_ registros por página",
